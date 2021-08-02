6a74b1926f5bede4cd804105cc4a2b699366da76
Your Name <yourname@yopmail.com>
2021-08-02 17:53:14 +0530

    Added fields to sign in page and added logout button into index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registration'}
   get 'countries/index'
-  root 'users#index'
+  root 'countries#index'
+
 
   #get 'delers/index'
  # get 'delers/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users 
+  #resources :users 
   resources :products
   resources :countries
   resources :product_types
